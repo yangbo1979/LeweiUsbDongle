0d739db64d35ca1abfcb519ed63ee1f866fafae7
yangbo1979 <gyangbo@gmail.com>
2014-12-12 09:28:23 +0800

    add support for lewei usb dongle2

--- usbdongle.py
+++ usbdongle.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import json
 
 class LeweiUsbDongle(object):
@@ -11,6 +12,9 @@ class LeweiUsbDongle(object):
         if(self.dongleType == '\x90\x01'):
             self.dongleName = "HT DEVICE"
             self.dongleDataLength = 2
+        if(self.dongleType == '\x90\x02'):
+            self.dongleName = "DUST DETECT DEVICE"
+            self.dongleDataLength = 4
         
     def hexShow(self,argv):  
         result = ''  
@@ -33,14 +37,16 @@ class LeweiUsbDongle(object):
     def dealHT(self,framedata):
         #print "Get datavalues from frame"
         humidityVal = (ord(framedata[0])*256+ord(framedata[1]))*0.1
-        print humidityVal
+        #print humidityVal
         temperatureVal = (ord(framedata[2])*256+ord(framedata[3]))*0.1
-        print temperatureVal
+        #print temperatureVal
 
         #define 'temperature,humidity' as name in Lewei50.com
         if(humidityVal > 0):
             sensor1={"Name":"temperature","Value":str(temperatureVal)}
-            sensor2={"Name":"humidity","Value":str(humidityVal)}
+            sensor2={"Name":"humidity","Value":str(humidityVal)}            
+            print "temperature:"+str(temperatureVal)
+            print "humidity:"+str(humidityVal) 
             lst_data=[]
             lst_data.append(sensor1)
             lst_data.append(sensor2)
@@ -48,11 +54,59 @@ class LeweiUsbDongle(object):
             #print (api_url)
             #res=PostData(apiUrl,userKey,usrData)
             return userData
+        
+    def dealDust(self,framedata):
+        #print "Get datavalues from frame"
+        humidityVal = (ord(framedata[0])*256+ord(framedata[1]))*0.1
+        #print humidityVal
+        temperatureVal = (ord(framedata[2])*256+ord(framedata[3]))*0.1
+        #print temperatureVal
+        vOut = (ord(framedata[4])*256+ord(framedata[5]))/1024*5
+        K = 0.17*1000
+        dustDensity = 0.56 * 1000
+        if(vOut > 0.325):
+            dustDensity = 0.56* 1000
+            exit()
+        elif(vOut < 0):
+            dustDensity = 0
+            exit()
+        else :
+            dustDensity = vOut * K
+        dustDensity = int(dustDensity)
+        #print "dustDensity:"+str(dustDensity)
+        pressureVal = (ord(framedata[6])*256+ord(framedata[7]))*0.1
+        #print pressureVal
+
+        #define 'temperature,humidity' as name in Lewei50.com.
+        lst_data=[]
+        if(humidityVal > 0 and humidityVal != 200):
+            sensor1={"Name":"temperature","Value":str(temperatureVal)}
+            sensor2={"Name":"humidity","Value":str(humidityVal)}
+            lst_data.append(sensor1)
+            lst_data.append(sensor2)
+            print "temperature:"+str(temperatureVal)
+            print "humidity:"+str(humidityVal)            
+        if(dustDensity > 0):
+            sensor3={"Name":"dust","Value":str(dustDensity)}
+            lst_data.append(sensor3)
+            print "dust:"+str(dustDensity)
+        if(pressureVal > 0):
+            sensor4={"Name":"pressure","Value":str(pressureVal)}
+            lst_data.append(sensor4)
+            print "pressure:"+str(pressureVal)
+            
+        userData=json.dumps(lst_data)
+            #print (api_url)
+            #res=PostData(apiUrl,userKey,usrData)
+            #return ""
+        return userData
             
     def handleData(self,framedata):
         data = ""
         if(self.dongleType == '\x90\x01'):
             data = self.dealHT(framedata)
+        if(self.dongleType == '\x90\x02'):
+            data = self.dealDust(framedata)
         print 'finished processing for device:'+self.dongleName
         return data
 
